51b5b8f163090bc753745b3691be4a91cdad89d8
jmerrill93 <merrill.joshua1@gmail.com>
2025-06-11 14:51:04 -0400

    added a --verbose flag

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai   
+from google import genai  
+from google.genai import types 
 
 # Load environment variables from .env file
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-# Generating content using the Gemini API
-
 if len(sys.argv) < 2:
     print ("Error: String required as command prompt input")
     sys.exit(1)
+# Generating content using the Gemini API
 
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents=sys.argv[1]
+    contents=messages
 )
 
-#printing response and token usage
 prompt_token = response.usage_metadata.prompt_token_count
 candidate_token = response.usage_metadata.candidates_token_count
-print (response.text)
-print (f"Prompt tokens: {prompt_token}")
-print (f"Response tokens: {candidate_token}")
+
+if len(sys.argv) == 2:
+    print (response.text)
+if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+    user_prompt = sys.argv[1]
+    print (f"User prompt: {user_prompt}")
+    print (f"Prompt tokens: {prompt_token}")
+    print (f"Response tokens: {candidate_token}")
+
