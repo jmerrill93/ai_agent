f050af2c706e166a4db5cde9ddb5ec76e1d16f91
jmerrill93 <merrill.joshua1@gmail.com>
2025-06-13 21:13:39 -0400

    added try-except block to get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,15 +6,21 @@ def get_files_info(working_directory, directory=None):
     if not os.path.commonpath([working_directory, directory]).startswith(os.path.abspath(working_directory)):
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
    
-    directory_list = os.listdir(directory)
-    new_list = []
-    for line in directory_list:
-        if os.path.isfile(os.path.join(directory, line)):
-            file_path = os.path.join(directory, line)
-            file_size = os.path.getsize(file_path)
-            new_list.append(f'- {line}: file_size={file_size} bytes, is_dir=False')
-        elif os.path.isdir(os.path.join(directory, line)):
-            file_path = os.path.join(directory, line)
-            file_size = os.path.getsize(file_path)
-            new_list.append(f'- {line}: file_size={file_size} bytes, is_dir=True')
-    return "\n".join(new_list)
+    try:
+        if not os.path.exists(directory):
+            raise FileNotFoundError(f"Error: File Not Found: {directory}")
+
+        directory_list = os.listdir(directory)
+        new_list = []
+        for line in directory_list:
+            if os.path.isfile(os.path.join(directory, line)):
+                file_path = os.path.join(directory, line)
+                file_size = os.path.getsize(file_path)
+                new_list.append(f'- {line}: file_size={file_size} bytes, is_dir=False')
+            elif os.path.isdir(os.path.join(directory, line)):
+                file_path = os.path.join(directory, line)
+                file_size = os.path.getsize(file_path)
+                new_list.append(f'- {line}: file_size={file_size} bytes, is_dir=True')
+        return "\n".join(new_list)
+    except Exception as e:
+        return f'Error: {str(e)}'
