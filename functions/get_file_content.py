7c1427e4ffa03090de609dcfcce15e69438c1930
jmerrill93 <merrill.joshua1@gmail.com>
2025-06-14 15:10:05 -0400

    simplifying get_file_contents.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,15 +3,16 @@ import os
 def get_file_content(working_directory, file_path):
     MAX_CHARS = 10000
     joined_path = os.path.join(working_directory, file_path)
-    if not os.path.isfile(joined_path):
-        return f'Error: File not found or is not a regular file: "{joined_path}"'
     if not os.path.abspath(joined_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(joined_path):
+        return f'Error: File not found or is not a regular file: "{joined_path}"'
+    
     try:
-        with open(file_path, "r") as f:
+        with open(joined_path, "r") as f:
 
             file_content_string = f.read(MAX_CHARS)
             if len(file_content_string) > MAX_CHARS:
-             print ("...File {file_path} truncated at 10000 characters")
+                return f'{file_content_string}'
     except Exception as e:
         return f'Error: {str(e)}'
